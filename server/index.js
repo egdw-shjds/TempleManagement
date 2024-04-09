ca083e4f201fe6aa78f79d41f3e64dd14221bd26
Shawn Joywin Dsouza <shjds@eg.dk>
2024-04-09 11:09:56 +0530

    changed route

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: ["https://temple-management-frontend.vercel.app/"],
+    origin: ["https://temple-management-frontend.vercel.app"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true
 }));
